4b533dec8443c636bd36e155513f8be9c4a1cb6e
Sébastien Brochet <sebastien.brochet@cern.ch>
2015-05-07 19:36:35 +0200

    Fix external build

--- external/build-external.sh
+++ external/build-external.sh
@@ -18,7 +18,7 @@ cd ../..
 rm yaml-cpp-0.5.1.tar.gz
 
 # TCLAP
-curl -O http://optimate.dl.sourceforge.net/project/tclap/tclap-1.2.1.tar.gz
+curl -L "http://downloads.sourceforge.net/project/tclap/{tclap-1.2.1.tar.gz}?r=http%3A%2F%2Fsourceforge.net%2Fprojects%2Ftclap%2Ffiles%2F&ts=1431017326&use_mirror=freefr" -o "#1"
 tar xf tclap-1.2.1.tar.gz
 
 cd tclap-1.2.1
